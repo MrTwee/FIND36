77a5bd8029c624fe29da3f9c807b902832cd7ad1
kukusum <zdrgnde@gmail.com>
2024-03-22 15:42:16 +0900

    clear

---
+++ final/build/CMakeFiles/FIND34.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kukusu/IoT3 Project/FIND36/final/Find36_GAME.c" "CMakeFiles/FIND34.dir/Find36_GAME.c.o" "gcc" "CMakeFiles/FIND34.dir/Find36_GAME.c.o.d"
+  "/home/kukusu/IoT3 Project/FIND36/final/Find36_MAIN.c" "CMakeFiles/FIND34.dir/Find36_MAIN.c.o" "gcc" "CMakeFiles/FIND34.dir/Find36_MAIN.c.o.d"
+  "/home/kukusu/IoT3 Project/FIND36/final/Find36_UT.c" "CMakeFiles/FIND34.dir/Find36_UT.c.o" "gcc" "CMakeFiles/FIND34.dir/Find36_UT.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ final/build/CMakeFiles/FIND34.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FIND34.dir/Find36_GAME.c.o"
+  "CMakeFiles/FIND34.dir/Find36_GAME.c.o.d"
+  "CMakeFiles/FIND34.dir/Find36_MAIN.c.o"
+  "CMakeFiles/FIND34.dir/Find36_MAIN.c.o.d"
+  "CMakeFiles/FIND34.dir/Find36_UT.c.o"
+  "CMakeFiles/FIND34.dir/Find36_UT.c.o.d"
+  "FIND34"
+  "FIND34.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FIND34.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
